82a210007ad43031ae0921938988ef0b171c8e5c
Sai Sandeep Nandyala <saisandeep99@gmail.com>
2026-02-07 23:39:06 -0500

    added new project

--- main.py
+++ main.py
@@ -1,6 +1,65 @@
-def main():
-    print("Hello from ncaiagents!")
+import dotenv
+
+dotenv.load_dotenv()
+
+from crewai import Crew, Agent, Task
+from crewai.project import CrewBase, agent, task, crew
+from tools import search_tool, scrape_tool
 
 
-if __name__ == "__main__":
-    main()
+@CrewBase
+class NewsReaderAgent:
+
+    @agent
+    def news_hunter_agent(self):
+        return Agent(
+            config=self.agents_config["news_hunter_agent"],
+            tools=[search_tool, scrape_tool],
+        )
+
+    @agent
+    def summarizer_agent(self):
+        return Agent(
+            config=self.agents_config["summarizer_agent"],
+            tools=[
+                scrape_tool,
+            ],
+        )
+
+    @agent
+    def curator_agent(self):
+        return Agent(
+            config=self.agents_config["curator_agent"],
+        )
+
+    @task
+    def content_harvesting_task(self):
+        return Task(
+            config=self.tasks_config["content_harvesting_task"],
+        )
+
+    @task
+    def summarization_task(self):
+        return Task(
+            config=self.tasks_config["summarization_task"],
+        )
+
+    @task
+    def final_report_assembly_task(self):
+        return Task(
+            config=self.tasks_config["final_report_assembly_task"],
+        )
+
+    @crew
+    def crew(self):
+        return Crew(
+            tasks=self.tasks,
+            agents=self.agents,
+            verbose=True,
+        )
+
+
+result = NewsReaderAgent().crew().kickoff(inputs={"topic": "Cambodia Thailand War."})
+
+for task_output in result.tasks_output:
+    print(task_output)
